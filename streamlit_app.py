95b0747348263a38366cf77d4fe6321c5a39a27c
Shardium <96217803+Shardium@users.noreply.github.com>
2025-06-08 23:15:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,6 +149,6 @@ if st.button("Classify"):
 st.markdown("""
     <hr style="margin-top: 50px;">
     <p style='text-align: center; color: grey;'>
-        Built using Streamlit | ⚠️ Please verify important info from trusted sources! <a href="https://github.com/Shardium/COVID-News-Classifier-with-BERT" rel="noopener noreferrer">GitHub</a>
+        Built using Streamlit | ⚠️ Please verify important info from trusted sources! | https://github.com/Shardium/COVID-News-Classifier-with-BERT
     </p>
 """, unsafe_allow_html=True)
